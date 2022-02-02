7e3f73a83247404513ca7980e3899a3923f6789d
Ayako Woollan <ayako.woollan@gmail.com>
2022-02-02 22:51:34 +1100

    updating all the test

---
+++ tests/Intern.test.js
@@ -0,0 +1,46 @@
+const Intern = require('../lib/Intern');
+
+describe('Intern constructor', () => {
+    it('assigns class attributes', () => {
+        const Ayako = new Intern('Ayako', '1', 'ayako@gmail.com');
+        expect(Ayako.name).toEqual('Ayako');
+        expect(Ayako.id).toEqual('1');
+        expect(Ayako.email).toEqual('ayako@gmail.com');
+
+    });
+
+    // it('returns error when the user inputs invalid value', () => {
+    //     const person = new Intern(' ', '-1', 'ayako@gmail.com');
+    //     expect(person.name).toEqual(`Expected parameter 'name' to be a non-empty string`);
+    //     expect(person.id).toEqual(`Expected parameter 'id' to be a non-negative number`);
+    //     expect(person.email).toEqual('ayako@gmail.com');
+    // })
+});
+
+describe('Intern methods', () => {
+    it('returns the correct name', () => {
+        const Ayako = new Intern('Ayako', '1', 'ayako@gmail.com');
+        expect(Ayako.getName()).toEqual('Ayako');
+
+    })
+
+    it('returns the correct id', () => {
+        const Ayako = new Intern('Ayako', '1', 'ayako@gmail.com');
+        expect(Ayako.getId()).toEqual('1');
+
+    })
+
+    it('returns the correct email', () => {
+        const Ayako = new Intern('Ayako', '1', 'ayako@gmail.com');
+        expect(Ayako.getEmail()).toEqual('ayako@gmail.com');
+
+    })
+
+    it('returns the correct role', () => {
+        const Ayako = new Intern('Ayako', '1', 'ayako@gmail.com');
+        expect(Ayako.getRole()).toEqual('Intern');
+
+    })
+
+
+})
--- tests/Manager.test.js
+++ tests/Manager.test.js
@@ -1,35 +1,35 @@
-const Employee = require('../lib/Employee');
+const Manager = require('../lib/Manager');
 
-describe('Employee constructor', () => {
+describe('Manager constructor', () => {
     it('assigns class attributes', () => {
-        const Ayako = new Employee('Ayako', '1', 'ayako@gmail.com');
+        const Ayako = new Manager('Ayako', '1', 'ayako@gmail.com');
         expect(Ayako.name).toEqual('Ayako');
         expect(Ayako.id).toEqual('1');
         expect(Ayako.email).toEqual('ayako@gmail.com');
     })
 });
 
-describe('Employee methods', () => {
+describe('Manager methods', () => {
     it('returns the correct name', () => {
-        const Ayako = new Employee('Ayako', '1', 'ayako@gmail.com');
+        const Ayako = new Manager('Ayako', '1', 'ayako@gmail.com');
         expect(Ayako.getName()).toEqual('Ayako');
 
     })
 
     it('returns the correct id', () => {
-        const Ayako = new Employee('Ayako', '1', 'ayako@gmail.com');
+        const Ayako = new Manager('Ayako', '1', 'ayako@gmail.com');
         expect(Ayako.getId()).toEqual('1');
 
     })
 
     it('returns the correct email', () => {
-        const Ayako = new Employee('Ayako', '1', 'ayako@gmail.com');
+        const Ayako = new Manager('Ayako', '1', 'ayako@gmail.com');
         expect(Ayako.getEmail()).toEqual('ayako@gmail.com');
 
     })
 
     it('returns the correct role', () => {
-        const Ayako = new Employee('Ayako', '1', 'ayako@gmail.com');
+        const Ayako = new Manager('Ayako', '1', 'ayako@gmail.com');
         expect(Ayako.getRole()).toEqual('Manager');
 
     })
