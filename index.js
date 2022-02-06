344f236c355996b46d1ab7649b4154dbaa66e957
Ayako Woollan <ayako.woollan@gmail.com>
2022-02-06 19:05:11 +1100

    updating generateHTML

--- index.js
+++ index.js
@@ -8,55 +8,37 @@ const Manager = require('./lib/Manager');
 const Intern = require('./lib/Intern');
 
 
+// const validate = {
+//     required: input => input !== '' ? true : "This field is required.",
+//     name: input => input !== '' ? true : "Please enter a name.",
+//     id: input => Number.isInteger(Number(input)) && Number(input) > 0 ? true : "Please enter a positive number.",
+//     email: input => input.match(/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+\@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/gi) ? true : "Please enter a valid email address.",
+// }
 
-const questions = [
-    {
-        type: '',
-        question: '',
-        name: '',
-    },
-    {
-        type: '',
-        question: '',
-        name: '',
-    },
-    {
-        type: '',
-        question: '',
-        name: '',
-    },
-    {
-        type: '',
-        question: '',
-        name: '',
-    },
-    {
-        type: '',
-        question: '',
-        name: '',
-    },
-    {
-        type: '',
-        question: '',
-        name: '',
-    },
-    {
-        type: '',
-        question: '',
-        name: '',
-    },
-    {
-        type: '',
-        question: '',
-        name: '',
-    },
-    {
-        type: '',
-        question: '',
-        name: '',
-    },
 
-]
+
+const initialQuestions = [
+    {
+        type: 'input',
+        question: 'What is your team name?',
+        name: 'teamName',
+    },
+    {
+        type: 'list',
+        question: 'Which type of team member would you like to add?',
+        name: 'type',
+        choices: ["Manager", "Intern", "Engineer", "Finish adding members"]
+
+    }]
+
+
+
+
+
+
+
+
+
 
 
 
@@ -66,7 +48,7 @@ Let's create your team profile website!
 If you would like to add a photo of your teammate, please save the photo with ID.png in assets folder.
 Please answer the questions bellow:
 `
-const promptUser = () => inquirer.prompt(questions);
+const promptUser = () => inquirer.prompt(initialQuestions);
 
 
 
